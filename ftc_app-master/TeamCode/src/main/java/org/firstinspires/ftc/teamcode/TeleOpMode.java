2a322299703eddfe2e3df1f586fad76b9ebd3724
cherma <cherma88@hotmail.com>
2016-11-29 15:38:55 -0800

    got rid of the update thing

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
@@ -1,7 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled; //WHAT THE HECK
+import com.qualcomm.robotcore.eventloop.opmode.Disabled; 
 
     @TeleOp(name="TeleOpMode", group="Opmode")
     public class TeleOpMode extends BotHardware
